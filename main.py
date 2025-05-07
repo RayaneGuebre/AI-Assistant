e31c97c888d40859f81fc6b070213a0075377841
Rayan <154925122+RayaneGuebre@users.noreply.github.com>
2025-05-07 12:50:23 +0000

    trying to add chatgpt

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-import openai
-import dotenv
+from openai import OpenAI
+
+client = OpenAI()
+import telegram
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 import whisper
 import requests
+
+# Load Env variables
+
+dotenv_path = join(dirname(__file__), ".env")
+load_dotenv(dotenv_path)
+OPENAI_KEY = os.environ.get("OPENAI_KEY")
+TELEGRAM_KEY = os.environ.get("TELEGRAM_BOT_KEY")
+TELEGRAM_ID = os.environ.get("TELEGRAM_ID")
+bot = telegram.Bot(token=TELEGRAM_KEY)
+
 # test voice recognition 
 #   model = whisper.load_model('base')
 #    result = model.transcribe('test_audio.m4a', fp16=False)
@@ -11,12 +26,13 @@ import requests
 
 # GPT integration
 
+
+sent_with_telegram = False
 def chat_with_gpt(prompt):
-  
-    response =  openai.chatCompletation.create(
-        model = "gpt-3.5-turbo",
-        message= [{"role" : "user", "content" : prompt}]
-    )
+
+    response =  client.chat.completions.create(
+    model = "gpt-3.5-turbo",
+    message= [{"role" : "user", "content" : prompt}])
     return response.choises[0].message.content.strip()
 
 if __name__ == "__main__":
@@ -25,4 +41,5 @@ if __name__ == "__main__":
         if user_input.lower in ["bye", "exit", "quit"]:
             break
         response = chat_with_gpt(user_input)
-        print(f"GPT: {response}")
+        if sent_with_telegram:
+            telegram.Chat.send_message(TELEGRAM_ID, response)
